d5d7b69e210243f453721f3eb05c3cd0dea6a6e9
aindriu80 <aindriu80@gmail.com>
2019-08-08 20:30:36 +0100

    Setting up the router package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+ 
+ 
 import './Blog.css';
 
 class Blog extends Component {
@@ -59,13 +58,7 @@ class Blog extends Component {
                 </header>
                 <section className="Posts">
                     {posts}                    
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                </section>              
             </div>
         );
     }
