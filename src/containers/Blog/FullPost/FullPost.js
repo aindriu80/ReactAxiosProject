80443c7b85089d05d94ad7ed63c1fc97da0ab0b4
aindriu80 <aindriu80@gmail.com>
2019-08-11 20:59:27 +0100

    Using the withRouter H.O.C & Route Props

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -7,6 +7,7 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
+    
     componentDidUpdate(){
         if (this.props.id){
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
