34d67b99f92c46253691eff8e03973d056081f1a
aindriu80 <aindriu80@gmail.com>
2019-08-13 20:30:52 +0100

    Navigating programmatically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -28,7 +28,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />             
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />               
                 <Route path="/" render={() => <h1>Home 2</h1>} />              */}
                 <Switch>
                     <Route path="/" exact component={Posts} />
