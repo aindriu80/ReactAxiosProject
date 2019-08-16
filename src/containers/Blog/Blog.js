d2929cb769906dd80aaf5734e93f61ad6b1361f7
aindriu80 <aindriu80@gmail.com>
2019-08-16 20:58:29 +0100

    Handing a 404 with a route without a path

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -35,8 +35,9 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth ? <Route path="/new-post" exact component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
+                    <Route render={()=> <h1>Not found</h1>}/>
                     {/* <Route path="/" component={Posts} /> */}
-                    <Redirect from="/" to="/posts" />
+                    {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
             </div>
         );
